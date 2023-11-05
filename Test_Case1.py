30153a16a7aed9eb7421d57e2e31a368742bc540
Mohammad.Faiz <faiz999888777@gmail.com>
2023-11-05 23:19:30 +0530

    updating readme

--- Test_Case1.py
+++ Test_Case1.py
@@ -46,3 +46,16 @@ time.sleep(2)
 #     print("Text is not available")
 #color: #6d737d;
 driver.quit()
+
+
+# #to convert rbga to hexadecimal
+# rgb_values = background_color_rgb.strip('rgba()').split(',') 
+# hex_color = "#{:02X}{:02X}{:02X}".format(int(rgb_values[0]), int(rgb_values[1]), int(rgb_values[2]))
+
+
+#to check the position of the element
+#element = driver.find_element_by_id('element_id')  
+# Replace with the appropriate method to locate your element 
+# Get the element's position element_position = element.location 
+# Extract X and Y coordinates x = element_position['x'] y = element_position['y'] 
+# Print the element's position print('Element Position (X, Y):', x, y)
