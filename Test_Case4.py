b6fef6e27bed0ce23c236c4cd6456afc4fbca44a
Mohammad.Faiz <faiz999888777@gmail.com>
2023-11-06 18:20:54 +0530

    Test case 4 completed

--- Test_Case4.py
+++ Test_Case4.py
@@ -10,9 +10,11 @@
 # Tableau
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium .webdriver.support .wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 
@@ -51,9 +53,87 @@ time.sleep(5)
 
 
 #select filter
-publication_date = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[5]/div[1]/div[4]/div[1]/div[1]")
+driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[1]/div[5]/div[1]/div[1]/button[1]").click()
 time.sleep(5)
-dropdown=Select(publication_date)
-dropdown.select_by_index(3)
+#select filter
+publication_date = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//div[@class='header'])[3]")))
+if publication_date.is_enabled():
+    publication_date.click()
+    time.sleep(5)
+    year =WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//div[normalize-space()='2021'])[1]")))
+    if year.is_enabled():
+        year.click()
+        time.sleep(5)
+# Check keywords in search bar
+Searchbar = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@name='query'])[1]")))
+if Searchbar.is_enabled():
+    Searchbar.click()
+    Searchbar.send_keys("Python")
+    time.sleep(5)
+    Search = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//i[@class='fa fa-search'])[1]")))
+    if Search.is_enabled():
+        Search.click()
+        time.sleep(5)
+    Searchfilter= WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@placeholder='Search titles …'])[2]")))
+    value = Searchfilter.get_attribute("value")
+    print(value)
+
+    if value == 'Python':
+        print('Keyword applied successfully')
+    else:
+        print(" Keyword not applied")
+
+Searchbar = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@name='query'])[1]")))
+if Searchbar.is_enabled():
+    Searchbar.click()
+    Searchbar.send_keys("Paint")
+    time.sleep(5)
+    Search = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//i[@class='fa fa-search'])[1]")))
+    if Search.is_enabled():
+        Search.click()
+        time.sleep(5)
+    Searchfilter= WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@placeholder='Search titles …'])[2]")))
+    value = Searchfilter.get_attribute("value")
+    print(value)
+
+    if value == 'Paint':
+        print('Keyword applied successfully')
+    else:
+        print(" Keyword not applied")
+
+Searchbar = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@name='query'])[1]")))
+if Searchbar.is_enabled():
+    Searchbar.click()
+    Searchbar.send_keys("Secure")
+    time.sleep(5)
+    Search = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//i[@class='fa fa-search'])[1]")))
+    if Search.is_enabled():
+        Search.click()
+        time.sleep(5)
+    Searchfilter= WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@placeholder='Search titles …'])[2]")))
+    value = Searchfilter.get_attribute("value")
+    print(value)
+
+    if value == 'Secure':
+        print('Keyword applied successfully')
+    else:
+        print(" Keyword not applied")
+Searchbar = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@name='query'])[1]")))
+if Searchbar.is_enabled():
+    Searchbar.click()
+    Searchbar.send_keys("Tableau")
+    time.sleep(5)
+    Search = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//i[@class='fa fa-search'])[1]")))
+    if Search.is_enabled():
+        Search.click()
+        time.sleep(5)
+    Searchfilter= WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//input[@placeholder='Search titles …'])[2]")))
+    value = Searchfilter.get_attribute("value")
+    print(value)
+
+    if value == 'Tableau':
+        print('Keyword applied successfully')
+    else:
+        print(" Keyword not applied")
 
 driver.quit()
