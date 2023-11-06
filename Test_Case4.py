9eff4e0ecf4207460ea7862c974934b503bc844a
Mohammad.Faiz <faiz999888777@gmail.com>
2023-11-06 15:55:58 +0530

    updateing test case 4

--- Test_Case4.py
+++ Test_Case4.py
@@ -1,7 +1,20 @@
+# Design a test that will click browse In the top nav, then click view all books. 
+# This should take you to the browse page. 
+# 
+# Click to clear any filters that are already set, and then click to set the 2021 filter for publication date. 
+# Following on from this, automate a test that will type the following words into the search bar, and
+# check that all titles found include that search text. 
+# Python 
+# Paint 
+# Secure 
+# Tableau
+
+import select
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 import time
 
 #driver
@@ -25,3 +38,23 @@ time.sleep(2)
 driver.find_element_by_xpath("//*[@id='login-form']/form/button[1]/span").click()
 time.sleep(5)
 print("Logged in successfully")
+time.sleep(5)
+
+
+#Cl̥ick browse
+driver.find_element_by_xpath("//*[@id='packt-navbar']/div[1]/a").click()
+print(driver.title) #Print title of the page
+time.sleep(5)
+
+#clear filter
+driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[5]/div[1]/div[1]/button[1]").click()
+time.sleep(5)
+
+
+#select filter
+publication_date = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[5]/div[1]/div[4]/div[1]/div[1]")
+time.sleep(5)
+dropdown=Select(publication_date)
+dropdown.select_by_index(3)
+
+driver.quit()
